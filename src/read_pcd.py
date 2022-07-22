3aea97fa5336a55baf6cf54d9fc2319c68b68b7c
hentaihusinsya <c1100781@planet.kanazawa-it.ac.jp>
2022-07-22 21:10:36 +0900

    Add: read_pcd.py

---
+++ src/read_pcd.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import rospy 
+import numpy as np
+import open3d as o3d
+import sensor_msgs.point_cloud2 as pc2
+from sensor_msgs.msg import PointCloud2
+
+def callback(data):
+
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber('/camera/depth/color/points', PointCloud2, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
+
+
