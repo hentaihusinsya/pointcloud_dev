6ba19169772ca66ce4d52d3bdefe3999070083a8
hentaihusinsya <c1100781@planet.kanazawa-it.ac.jp>
2022-07-24 02:21:21 +0900

    improve read_pcd.py

--- src/read_pcd.py
+++ src/read_pcd.py
@@ -6,26 +6,30 @@ import numpy as np
 import open3d as o3d
 import sensor_msgs.point_cloud2 as pc2
 from sensor_msgs.msg import PointCloud2
+from std_msgs.msg import MultiArrayDimension, Float32MultiArray
+class read_pcd:
 
-def callback(data):
+    def callback(self, data):
+        point_cloud_list = pc2.read_points_list(data)
+        #print(point_cloud_list)
+        point_cloud_numpy = np.array(point_cloud_list)
+        print(point_cloud_numpy.shape)
 
-    point_cloud_list = pc2.read_points_list(data)
-    #print(point_cloud_list)
-    point_cloud_numpy = np.array(point_cloud_list)
-    print(point_cloud_numpy.shape)
-    
-    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    def listener(self):
+        rospy.init_node('listener', anonymous=True)
 
-def listener():
-    rospy.init_node('listener', anonymous=True)
+        rospy.Subscriber('/camera/depth/color/points', PointCloud2, self.callback)
 
-    rospy.Subscriber('/camera/depth/color/points', PointCloud2, callback)
+        rospy.spin()
 
-    rospy.spin()
+    def publisher(self):
+        pub = rospy.Publisher('pointcloud_data')
 
 
 
 if __name__ == '__main__':
-    listener()
+    a =  read_pcd()
+    a.listener()
+
 
 
